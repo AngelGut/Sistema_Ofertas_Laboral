5f3c3e164f447be0935234633d1ec6b88e40f535
AngelG <angeldavid.guti.con@gmail.com>
2025-07-03 12:01:33 -0400

    planteamiento de la clase persona

--- CpNegocio/Base/Persona.cs
+++ CpNegocio/Base/Persona.cs
@@ -10,7 +10,24 @@ namespace CpNegocio.Base
 {
     public class Persona : BaseUser
     {
+        //Campos privados para la clase Person
+        private string _nombre;
+        private string _id;
+        private string _telefono;
+        private string _correo;
 
+        public string Nombre 
+        {
+            get { return _nombre; }
+            set { _nombre = value; } 
+        }
+        public string Id 
+        { 
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
 
     }
 }
