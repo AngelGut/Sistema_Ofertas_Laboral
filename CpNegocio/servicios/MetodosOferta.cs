b52b46a120a667a51a266bfe2d36d7ed9a668ebf
AngelG <angeldavid.guti.con@gmail.com>
2025-07-07 21:47:16 -0400

    Reorganizacion de los metodos

---
+++ CpNegocio/servicios/MetodosOferta.cs
@@ -0,0 +1,18 @@
+﻿using Capa_Datos;
+using CpNegocio.Oferta;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpNegocio.servicios
+{
+    public class MetodosOFerta : MetodosBase
+    {
+        
+    }
+}
+
