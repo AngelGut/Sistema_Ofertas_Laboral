6ac16a46509871311eacbbc43ac923024f072504
Eliancmercedes <elianrd10@icloud.com>
2025-07-31 02:34:28 -0800

    Primera actualizacion form de Elian (tengo que hablar algo con el lidel) xD

--- CpPresentacion/Empleo.Designer.cs
+++ CpPresentacion/Empleo.Designer.cs
@@ -34,7 +34,14 @@
             tabPage3 = new TabPage();
             tabPage4 = new TabPage();
             tabPage5 = new TabPage();
+            txtBuscarID = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            txtBuscarDNI = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            dgvEmpresas = new DataGridView();
+            dgvPostulantes = new DataGridView();
             materialTabControl1.SuspendLayout();
+            tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEmpresas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPostulantes).BeginInit();
             SuspendLayout();
             // 
             // materialTabControl1
@@ -46,70 +53,187 @@
             materialTabControl1.Controls.Add(tabPage5);
             materialTabControl1.Depth = 0;
             materialTabControl1.Dock = DockStyle.Fill;
-            materialTabControl1.Location = new Point(3, 64);
+            materialTabControl1.Location = new Point(2, 51);
+            materialTabControl1.Margin = new Padding(2);
             materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             materialTabControl1.Multiline = true;
             materialTabControl1.Name = "materialTabControl1";
             materialTabControl1.SelectedIndex = 0;
-            materialTabControl1.Size = new Size(1547, 850);
+            materialTabControl1.Size = new Size(1238, 681);
             materialTabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 34);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Margin = new Padding(2);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1539, 812);
+            tabPage1.Padding = new Padding(2);
+            tabPage1.Size = new Size(1230, 648);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Menu";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 34);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Margin = new Padding(2);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1539, 812);
+            tabPage2.Padding = new Padding(2);
+            tabPage2.Size = new Size(1230, 648);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Ofertas Laborales";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 34);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Margin = new Padding(2);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(1539, 812);
+            tabPage3.Size = new Size(1230, 648);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Empresas";
             tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            tabPage4.Location = new Point(4, 34);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Margin = new Padding(2);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(1539, 812);
+            tabPage4.Size = new Size(1230, 648);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "Postulantes";
             tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            tabPage5.Location = new Point(4, 34);
+            tabPage5.Controls.Add(txtBuscarID);
+            tabPage5.Controls.Add(txtBuscarDNI);
+            tabPage5.Controls.Add(dgvEmpresas);
+            tabPage5.Controls.Add(dgvPostulantes);
+            tabPage5.Location = new Point(4, 29);
+            tabPage5.Margin = new Padding(2);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(1539, 812);
+            tabPage5.Size = new Size(1230, 648);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "Empleo";
             tabPage5.UseVisualStyleBackColor = true;
             // 
+            // txtBuscarID
+            // 
+            txtBuscarID.AllowPromptAsInput = true;
+            txtBuscarID.AnimateReadOnly = false;
+            txtBuscarID.AsciiOnly = false;
+            txtBuscarID.BackgroundImageLayout = ImageLayout.None;
+            txtBuscarID.BeepOnError = false;
+            txtBuscarID.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarID.Depth = 0;
+            txtBuscarID.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtBuscarID.HidePromptOnLeave = false;
+            txtBuscarID.HideSelection = true;
+            txtBuscarID.InsertKeyMode = InsertKeyMode.Default;
+            txtBuscarID.LeadingIcon = null;
+            txtBuscarID.Location = new Point(789, 470);
+            txtBuscarID.Mask = "";
+            txtBuscarID.MaxLength = 32767;
+            txtBuscarID.MouseState = MaterialSkin.MouseState.OUT;
+            txtBuscarID.Name = "txtBuscarID";
+            txtBuscarID.PasswordChar = '\0';
+            txtBuscarID.PrefixSuffixText = null;
+            txtBuscarID.PromptChar = '_';
+            txtBuscarID.ReadOnly = false;
+            txtBuscarID.RejectInputOnFirstFailure = false;
+            txtBuscarID.ResetOnPrompt = true;
+            txtBuscarID.ResetOnSpace = true;
+            txtBuscarID.RightToLeft = RightToLeft.No;
+            txtBuscarID.SelectedText = "";
+            txtBuscarID.SelectionLength = 0;
+            txtBuscarID.SelectionStart = 0;
+            txtBuscarID.ShortcutsEnabled = true;
+            txtBuscarID.Size = new Size(312, 48);
+            txtBuscarID.SkipLiterals = true;
+            txtBuscarID.TabIndex = 3;
+            txtBuscarID.TabStop = false;
+            txtBuscarID.Text = "Buscar por ID";
+            txtBuscarID.TextAlign = HorizontalAlignment.Left;
+            txtBuscarID.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarID.TrailingIcon = null;
+            txtBuscarID.UseSystemPasswordChar = false;
+            txtBuscarID.ValidatingType = null;
+            // 
+            // txtBuscarDNI
+            // 
+            txtBuscarDNI.AllowPromptAsInput = true;
+            txtBuscarDNI.AnimateReadOnly = false;
+            txtBuscarDNI.AsciiOnly = false;
+            txtBuscarDNI.BackgroundImageLayout = ImageLayout.None;
+            txtBuscarDNI.BeepOnError = false;
+            txtBuscarDNI.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarDNI.Depth = 0;
+            txtBuscarDNI.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtBuscarDNI.HidePromptOnLeave = false;
+            txtBuscarDNI.HideSelection = true;
+            txtBuscarDNI.InsertKeyMode = InsertKeyMode.Default;
+            txtBuscarDNI.LeadingIcon = null;
+            txtBuscarDNI.Location = new Point(191, 470);
+            txtBuscarDNI.Mask = "";
+            txtBuscarDNI.MaxLength = 32767;
+            txtBuscarDNI.MouseState = MaterialSkin.MouseState.OUT;
+            txtBuscarDNI.Name = "txtBuscarDNI";
+            txtBuscarDNI.PasswordChar = '\0';
+            txtBuscarDNI.PrefixSuffixText = null;
+            txtBuscarDNI.PromptChar = '_';
+            txtBuscarDNI.ReadOnly = false;
+            txtBuscarDNI.RejectInputOnFirstFailure = false;
+            txtBuscarDNI.ResetOnPrompt = true;
+            txtBuscarDNI.ResetOnSpace = true;
+            txtBuscarDNI.RightToLeft = RightToLeft.No;
+            txtBuscarDNI.SelectedText = "";
+            txtBuscarDNI.SelectionLength = 0;
+            txtBuscarDNI.SelectionStart = 0;
+            txtBuscarDNI.ShortcutsEnabled = true;
+            txtBuscarDNI.Size = new Size(312, 48);
+            txtBuscarDNI.SkipLiterals = true;
+            txtBuscarDNI.TabIndex = 2;
+            txtBuscarDNI.TabStop = false;
+            txtBuscarDNI.Text = "Buscar por DNI";
+            txtBuscarDNI.TextAlign = HorizontalAlignment.Left;
+            txtBuscarDNI.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarDNI.TrailingIcon = null;
+            txtBuscarDNI.UseSystemPasswordChar = false;
+            txtBuscarDNI.ValidatingType = null;
+            // 
+            // dgvEmpresas
+            // 
+            dgvEmpresas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEmpresas.Location = new Point(637, 50);
+            dgvEmpresas.Name = "dgvEmpresas";
+            dgvEmpresas.RowHeadersWidth = 51;
+            dgvEmpresas.Size = new Size(577, 275);
+            dgvEmpresas.TabIndex = 1;
+            // 
+            // dgvPostulantes
+            // 
+            dgvPostulantes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvPostulantes.Location = new Point(25, 50);
+            dgvPostulantes.Name = "dgvPostulantes";
+            dgvPostulantes.RowHeadersWidth = 51;
+            dgvPostulantes.Size = new Size(567, 275);
+            dgvPostulantes.TabIndex = 0;
+            // 
             // Empleo
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1553, 917);
+            ClientSize = new Size(1242, 734);
             Controls.Add(materialTabControl1);
+            Margin = new Padding(2);
             Name = "Empleo";
+            Padding = new Padding(2, 51, 2, 2);
             Text = "Empleo";
             materialTabControl1.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvEmpresas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPostulantes).EndInit();
             ResumeLayout(false);
         }
 
@@ -121,5 +245,9 @@
         private TabPage tabPage3;
         private TabPage tabPage4;
         private TabPage tabPage5;
+        private DataGridView dgvEmpresas;
+        private DataGridView dgvPostulantes;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtBuscarID;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtBuscarDNI;
     }
 }
