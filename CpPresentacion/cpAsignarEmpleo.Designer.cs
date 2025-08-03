0f97d08b92a4f1bb52144ddfb0143f846f693de2
Eliancmercedes <elianrd10@icloud.com>
2025-08-03 12:17:17 -0800

    Agregado de filtro para los postulantes

--- CpPresentacion/cpAsignarEmpleo.Designer.cs
+++ CpPresentacion/cpAsignarEmpleo.Designer.cs
@@ -28,16 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
             tabPage1 = new TabPage();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
             tabPage4 = new TabPage();
             tabPage5 = new TabPage();
+            cmbFiltroBusqueda = new ComboBox();
+            btnBuscarID = new MaterialSkin.Controls.MaterialButton();
+            txtBuscarID = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            txtBuscarDNI = new MaterialSkin.Controls.MaterialMaskedTextBox();
+            dgvEmpresas = new DataGridView();
+            dgvPostulantes = new DataGridView();
             tabPage6 = new TabPage();
             tabPage7 = new TabPage();
             tabPage8 = new TabPage();
             materialTabControl1.SuspendLayout();
+            tabPage5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEmpresas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPostulantes).BeginInit();
             SuspendLayout();
             // 
             // materialTabControl1
@@ -52,101 +63,278 @@
             materialTabControl1.Controls.Add(tabPage8);
             materialTabControl1.Depth = 0;
             materialTabControl1.Dock = DockStyle.Fill;
-            materialTabControl1.Location = new Point(3, 64);
+            materialTabControl1.Location = new Point(2, 51);
+            materialTabControl1.Margin = new Padding(2);
             materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             materialTabControl1.Multiline = true;
             materialTabControl1.Name = "materialTabControl1";
             materialTabControl1.SelectedIndex = 0;
-            materialTabControl1.Size = new Size(2121, 1075);
+            materialTabControl1.Size = new Size(1550, 829);
             materialTabControl1.TabIndex = 1;
             materialTabControl1.SelectedIndexChanged += materialTabControl1_SelectedIndexChanged;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 34);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Margin = new Padding(2);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(2113, 1037);
+            tabPage1.Padding = new Padding(2);
+            tabPage1.Size = new Size(1542, 796);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Menu";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 34);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Margin = new Padding(2);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(2113, 1037);
+            tabPage2.Padding = new Padding(2);
+            tabPage2.Size = new Size(1542, 796);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Ofertas Laborales";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 34);
+            tabPage3.Location = new Point(4, 29);
+            tabPage3.Margin = new Padding(2);
             tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(2113, 1037);
+            tabPage3.Padding = new Padding(2);
+            tabPage3.Size = new Size(1542, 796);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Empresas";
             tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            tabPage4.Location = new Point(4, 34);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Margin = new Padding(2);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(2113, 1037);
+            tabPage4.Size = new Size(1542, 796);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "Postulantes";
             tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            tabPage5.Location = new Point(4, 34);
+            tabPage5.Controls.Add(cmbFiltroBusqueda);
+            tabPage5.Controls.Add(btnBuscarID);
+            tabPage5.Controls.Add(txtBuscarID);
+            tabPage5.Controls.Add(txtBuscarDNI);
+            tabPage5.Controls.Add(dgvEmpresas);
+            tabPage5.Controls.Add(dgvPostulantes);
+            tabPage5.Location = new Point(4, 29);
+            tabPage5.Margin = new Padding(2);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(2113, 1037);
+            tabPage5.Size = new Size(1542, 796);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "Asignar Oferta";
             tabPage5.UseVisualStyleBackColor = true;
             // 
+            // cmbFiltroBusqueda
+            // 
+            cmbFiltroBusqueda.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbFiltroBusqueda.FormattingEnabled = true;
+            cmbFiltroBusqueda.Location = new Point(37, 383);
+            cmbFiltroBusqueda.Name = "cmbFiltroBusqueda";
+            cmbFiltroBusqueda.Size = new Size(151, 28);
+            cmbFiltroBusqueda.TabIndex = 5;
+            // 
+            // btnBuscarID
+            // 
+            btnBuscarID.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnBuscarID.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            btnBuscarID.Depth = 0;
+            btnBuscarID.HighEmphasis = true;
+            btnBuscarID.Icon = null;
+            btnBuscarID.Location = new Point(345, 466);
+            btnBuscarID.Margin = new Padding(4, 6, 4, 6);
+            btnBuscarID.MouseState = MaterialSkin.MouseState.HOVER;
+            btnBuscarID.Name = "btnBuscarID";
+            btnBuscarID.NoAccentTextColor = Color.Empty;
+            btnBuscarID.Size = new Size(77, 36);
+            btnBuscarID.TabIndex = 4;
+            btnBuscarID.Text = "Buscar";
+            btnBuscarID.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            btnBuscarID.UseAccentColor = false;
+            btnBuscarID.UseVisualStyleBackColor = true;
+            btnBuscarID.Click += btnBuscarID_Click;
+            // 
+            // txtBuscarID
+            // 
+            txtBuscarID.AllowPromptAsInput = true;
+            txtBuscarID.AnimateReadOnly = false;
+            txtBuscarID.AsciiOnly = false;
+            txtBuscarID.BackgroundImageLayout = ImageLayout.None;
+            txtBuscarID.BeepOnError = false;
+            txtBuscarID.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarID.Depth = 0;
+            txtBuscarID.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtBuscarID.HidePromptOnLeave = false;
+            txtBuscarID.HideSelection = true;
+            txtBuscarID.InsertKeyMode = InsertKeyMode.Default;
+            txtBuscarID.LeadingIcon = null;
+            txtBuscarID.Location = new Point(717, 417);
+            txtBuscarID.Mask = "";
+            txtBuscarID.MaxLength = 32767;
+            txtBuscarID.MouseState = MaterialSkin.MouseState.OUT;
+            txtBuscarID.Name = "txtBuscarID";
+            txtBuscarID.PasswordChar = '\0';
+            txtBuscarID.PrefixSuffixText = null;
+            txtBuscarID.PromptChar = '_';
+            txtBuscarID.ReadOnly = false;
+            txtBuscarID.RejectInputOnFirstFailure = false;
+            txtBuscarID.ResetOnPrompt = true;
+            txtBuscarID.ResetOnSpace = true;
+            txtBuscarID.RightToLeft = RightToLeft.No;
+            txtBuscarID.SelectedText = "";
+            txtBuscarID.SelectionLength = 0;
+            txtBuscarID.SelectionStart = 0;
+            txtBuscarID.ShortcutsEnabled = true;
+            txtBuscarID.Size = new Size(312, 48);
+            txtBuscarID.SkipLiterals = true;
+            txtBuscarID.TabIndex = 3;
+            txtBuscarID.TabStop = false;
+            txtBuscarID.Text = "materialMaskedTextBox2";
+            txtBuscarID.TextAlign = HorizontalAlignment.Left;
+            txtBuscarID.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarID.TrailingIcon = null;
+            txtBuscarID.UseSystemPasswordChar = false;
+            txtBuscarID.ValidatingType = null;
+            // 
+            // txtBuscarDNI
+            // 
+            txtBuscarDNI.AllowPromptAsInput = true;
+            txtBuscarDNI.AnimateReadOnly = false;
+            txtBuscarDNI.AsciiOnly = false;
+            txtBuscarDNI.BackgroundImageLayout = ImageLayout.None;
+            txtBuscarDNI.BeepOnError = false;
+            txtBuscarDNI.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarDNI.Depth = 0;
+            txtBuscarDNI.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtBuscarDNI.HidePromptOnLeave = false;
+            txtBuscarDNI.HideSelection = true;
+            txtBuscarDNI.InsertKeyMode = InsertKeyMode.Default;
+            txtBuscarDNI.LeadingIcon = null;
+            txtBuscarDNI.Location = new Point(24, 466);
+            txtBuscarDNI.Mask = "";
+            txtBuscarDNI.MaxLength = 32767;
+            txtBuscarDNI.MouseState = MaterialSkin.MouseState.OUT;
+            txtBuscarDNI.Name = "txtBuscarDNI";
+            txtBuscarDNI.PasswordChar = '\0';
+            txtBuscarDNI.PrefixSuffixText = null;
+            txtBuscarDNI.PromptChar = '_';
+            txtBuscarDNI.ReadOnly = false;
+            txtBuscarDNI.RejectInputOnFirstFailure = false;
+            txtBuscarDNI.ResetOnPrompt = true;
+            txtBuscarDNI.ResetOnSpace = true;
+            txtBuscarDNI.RightToLeft = RightToLeft.No;
+            txtBuscarDNI.SelectedText = "";
+            txtBuscarDNI.SelectionLength = 0;
+            txtBuscarDNI.SelectionStart = 0;
+            txtBuscarDNI.ShortcutsEnabled = true;
+            txtBuscarDNI.Size = new Size(286, 48);
+            txtBuscarDNI.SkipLiterals = true;
+            txtBuscarDNI.TabIndex = 2;
+            txtBuscarDNI.TabStop = false;
+            txtBuscarDNI.TextAlign = HorizontalAlignment.Left;
+            txtBuscarDNI.TextMaskFormat = MaskFormat.IncludeLiterals;
+            txtBuscarDNI.TrailingIcon = null;
+            txtBuscarDNI.UseSystemPasswordChar = false;
+            txtBuscarDNI.ValidatingType = null;
+            // 
+            // dgvEmpresas
+            // 
+            dgvEmpresas.AllowUserToAddRows = false;
+            dgvEmpresas.AllowUserToDeleteRows = false;
+            dgvEmpresas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Window;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(192, 0, 192);
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dgvEmpresas.DefaultCellStyle = dataGridViewCellStyle1;
+            dgvEmpresas.Location = new Point(635, 38);
+            dgvEmpresas.Name = "dgvEmpresas";
+            dgvEmpresas.ReadOnly = true;
+            dgvEmpresas.RowHeadersWidth = 51;
+            dgvEmpresas.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvEmpresas.Size = new Size(565, 294);
+            dgvEmpresas.TabIndex = 1;
+            dgvEmpresas.CellContentClick += dgvEmpresas_CellContentClick;
+            // 
+            // dgvPostulantes
+            // 
+            dgvPostulantes.AllowUserToAddRows = false;
+            dgvPostulantes.AllowUserToDeleteRows = false;
+            dgvPostulantes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(192, 0, 192);
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvPostulantes.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvPostulantes.Location = new Point(24, 38);
+            dgvPostulantes.Name = "dgvPostulantes";
+            dgvPostulantes.ReadOnly = true;
+            dgvPostulantes.RowHeadersWidth = 51;
+            dgvPostulantes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvPostulantes.Size = new Size(527, 294);
+            dgvPostulantes.TabIndex = 0;
+            dgvPostulantes.CellContentClick += dgvPostulantes_CellContentClick;
+            // 
             // tabPage6
             // 
-            tabPage6.Location = new Point(4, 34);
+            tabPage6.Location = new Point(4, 29);
+            tabPage6.Margin = new Padding(2);
             tabPage6.Name = "tabPage6";
-            tabPage6.Size = new Size(2113, 1037);
+            tabPage6.Size = new Size(1542, 796);
             tabPage6.TabIndex = 5;
             tabPage6.Text = "Historial Correos";
             tabPage6.UseVisualStyleBackColor = true;
             // 
             // tabPage7
             // 
-            tabPage7.Location = new Point(4, 34);
+            tabPage7.Location = new Point(4, 29);
+            tabPage7.Margin = new Padding(2);
             tabPage7.Name = "tabPage7";
-            tabPage7.Size = new Size(2113, 1037);
+            tabPage7.Size = new Size(1542, 796);
             tabPage7.TabIndex = 6;
             tabPage7.Text = "Carnet";
             tabPage7.UseVisualStyleBackColor = true;
             // 
             // tabPage8
             // 
-            tabPage8.Location = new Point(4, 34);
+            tabPage8.Location = new Point(4, 29);
+            tabPage8.Margin = new Padding(2);
             tabPage8.Name = "tabPage8";
-            tabPage8.Size = new Size(2113, 1037);
+            tabPage8.Size = new Size(1542, 796);
             tabPage8.TabIndex = 7;
             tabPage8.Text = "Registro Interno";
             tabPage8.UseVisualStyleBackColor = true;
             // 
             // cpAsignarEmpleo
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(2127, 1142);
+            ClientSize = new Size(1554, 882);
             Controls.Add(materialTabControl1);
             DrawerTabControl = materialTabControl1;
+            Margin = new Padding(2);
             Name = "cpAsignarEmpleo";
+            Padding = new Padding(2, 51, 2, 2);
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Asignar Oferta";
             materialTabControl1.ResumeLayout(false);
+            tabPage5.ResumeLayout(false);
+            tabPage5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvEmpresas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvPostulantes).EndInit();
             ResumeLayout(false);
         }
 
@@ -161,5 +349,11 @@
         private TabPage tabPage6;
         private TabPage tabPage7;
         private TabPage tabPage8;
+        private DataGridView dgvPostulantes;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtBuscarID;
+        private MaterialSkin.Controls.MaterialMaskedTextBox txtBuscarDNI;
+        private DataGridView dgvEmpresas;
+        private MaterialSkin.Controls.MaterialButton btnBuscarID;
+        private ComboBox cmbFiltroBusqueda;
     }
 }
--- CpPresentacion/cpAsignarEmpleo.cs
+++ CpPresentacion/cpAsignarEmpleo.cs
@@ -1,4 +1,6 @@
-﻿using MaterialSkin.Controls;
+﻿using CpNegocio.Empresas_y_Postulantes;
+using CpNegocio;
+using MaterialSkin.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,17 +14,221 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CpPresentacion
 {
-    public partial class cpAsignarEmpleo : MaterialForm // <<== ¡Cambiado a MaterialForm!
+    public partial class cpAsignarEmpleo : MaterialForm
     {
+        private DataTable tablaPostulantes;
+        private int idPostulanteSeleccionado = -1;
+        private int idEmpresaSeleccionada = -1;
+
+
         public cpAsignarEmpleo()
         {
             InitializeComponent();
-
             materialTabControl1.SelectedIndex = 4;
+            this.Load += frmEmpleo_Load;
+            cmbFiltroBusqueda.Items.Clear();
+            cmbFiltroBusqueda.Items.Add("ID");
+            cmbFiltroBusqueda.Items.Add("Cédula");
+            cmbFiltroBusqueda.SelectedIndex = 0;
+            cmbFiltroBusqueda.SelectedIndexChanged += cmbFiltroBusqueda_SelectedIndexChanged;
+
+        }
+
+        /* Evento Load para inicializar datos */
+        private void frmEmpleo_Load(object sender, EventArgs e)
+        {
+            MostrarPostulantes();
+            MostrarEmpresas();
+        }
+
+        private void MostrarPostulantes()
+        {
+            var negocioPostulante = new NPostulante();
+            tablaPostulantes = negocioPostulante.Mostrar();
+            dgvPostulantes.DataSource = tablaPostulantes;
+        }
+
+        private void BuscarPostulantePorID(string id)
+        {
+            var negocioPostulante = new NPostulante();
+            dgvPostulantes.DataSource = negocioPostulante.BuscarPorID(id);
+        }
+
+        private void BuscarPostulantePorDNI(string dni)
+        {
+            var negocioPostulante = new NPostulante();
+            dgvPostulantes.DataSource = negocioPostulante.BuscarPorDNI(dni);
+        }
+
+        private void MostrarEmpresas()
+        {
+            var negocioEmpresa = new NEmpresa();
+            dgvEmpresas.DataSource = negocioEmpresa.Mostrar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string texto = txtBuscarDNI.Text.Trim();
+            if (string.IsNullOrEmpty(texto))
+            {
+                dgvPostulantes.DataSource = tablaPostulantes;
+                return;
+            }
+
+            if (cmbFiltroBusqueda.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecciona un filtro: ID o Cédula.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string opcion = cmbFiltroBusqueda.SelectedItem.ToString();
+            DataView vista = new DataView(tablaPostulantes);
+
+            string filtro = opcion switch
+            {
+                "ID" => $"Convert(Id, 'System.String') = '{texto}'",
+                "Cédula" => $"Cedula LIKE '%{texto}%'",
+                _ => ""
+            };
+
+            if (string.IsNullOrEmpty(filtro))
+            {
+                MessageBox.Show("Filtro inválido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            vista.RowFilter = filtro;
+            dgvPostulantes.DataSource = vista;
+
+            // ✅ Verificamos si la vista está vacía DESPUÉS de aplicar el filtro
+            if (vista.Count == 0)
+            {
+                MessageBox.Show($"No se encontró ningún postulante con {opcion}: {texto}", "Sin resultados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgvPostulantes.DataSource = tablaPostulantes; // (opcional) restaurar lista completa
+            }
         }
 
 
-        /* Conecta este handler en el diseñador (⚡ SelectedIndexChanged) */
+
+        private void btnBuscarID_Click(object sender, EventArgs e)
+        {
+            string texto = txtBuscarDNI.Text;
+
+
+
+
+
+
+            if (string.IsNullOrEmpty(texto))
+            {
+                dgvPostulantes.DataSource = tablaPostulantes;
+                return;
+            }
+
+            if (cmbFiltroBusqueda.SelectedItem == null)
+            {
+                MessageBox.Show("Por favor, selecciona un filtro: ID o Cédula.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string opcion = cmbFiltroBusqueda.SelectedItem.ToString();
+            DataView vista = new DataView(tablaPostulantes);
+
+            string filtro = opcion switch
+            {
+                "ID" => $"Convert(Id, 'System.String') = '{texto}'",
+                "Cédula" => $"Cedula LIKE '%{texto}%'",
+                _ => ""
+            };
+
+            
+
+            if (string.IsNullOrEmpty(filtro))
+            {
+                MessageBox.Show("Filtro inválido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            vista.RowFilter = filtro;
+            dgvPostulantes.DataSource = vista;
+
+            if (vista.Count == 0)
+            {
+                MessageBox.Show($"No se encontró ningún postulante con {opcion}.", "Sin resultados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgvPostulantes.DataSource = tablaPostulantes;
+            }
+        }
+
+
+
+
+
+        private void txtBuscarDNI_TextChanged(object sender, EventArgs e)
+        {
+            FiltrarPostulantes(txtBuscarDNI.Text.Trim());
+        }
+
+
+        private void FiltrarPostulantes(string texto)
+        {
+            if (tablaPostulantes == null) return;
+            if (string.IsNullOrWhiteSpace(texto))
+            {
+                dgvPostulantes.DataSource = tablaPostulantes;
+                return;
+            }
+
+            DataView vista = new DataView(tablaPostulantes);
+            string filtro = "";
+
+            string filtroSeleccionado = cmbFiltroBusqueda.SelectedItem?.ToString();
+
+            if (filtroSeleccionado == "ID")
+            {
+                // Busca exacto o parcial en ID (puedes ajustar para exacto si querés)
+                filtro = $"Convert(Id, 'System.String') LIKE '%{texto}%'";
+            }
+            else if (filtroSeleccionado == "Cédula")
+            {
+                // Busca texto parcial en Cédula
+                filtro = $"Cedula LIKE '%{texto}%'";
+            }
+            else
+            {
+                // Si no hay selección válida, no filtrar o filtrar en ambas
+                filtro = $"Convert(Id, 'System.String') LIKE '%{texto}%' OR Cedula LIKE '%{texto}%'";
+            }
+
+            vista.RowFilter = filtro;
+            dgvPostulantes.DataSource = vista;
+        }
+        private string cedulaPostulanteSeleccionado;
+        private string correoEmpresaSeleccionada;
+
+        private void cmbFiltroBusqueda_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FiltrarPostulantes(txtBuscarDNI.Text.Trim());
+        }
+
+
+
+
+        private void txtBuscarDNI_Click(object sender, EventArgs e)
+        {
+            // Código opcional al hacer click en el TextBox
+        }
+
+        private void txtBuscarID_Click(object sender, EventArgs e)
+        {
+            // Código opcional al hacer click en el TextBox
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+            // Evento de clic en la pestaña 3 (si aplica)
+        }
+
+        /* Manejo del cambio de pestañas */
         private async void materialTabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
             await NavegarA(materialTabControl1.SelectedIndex);
@@ -30,13 +236,9 @@ namespace CpPresentacion
 
         private async Task NavegarA(int idx)
         {
-            // A) ¿A qué ventana ir?
             Form destino = idx switch
             {
-                0 => Application.OpenForms.OfType<Menu>()
-                                          .FirstOrDefault() ?? new Menu(),
-
-                // Evitamos duplicar instancias si ya estamos ahí
+                0 => Application.OpenForms.OfType<Menu>().FirstOrDefault() ?? new Menu(),
                 1 => this is cpOfertas ? this : new cpOfertas(),
                 2 => this is cpEmpresa ? this : new cpEmpresa(),
                 3 => this is cpPostulante ? this : new cpPostulante(),
@@ -47,21 +249,38 @@ namespace CpPresentacion
                 _ => null
             };
 
-            // B) Si ya estamos en el destino, no hacemos nada
             if (destino == null || destino == this) return;
 
-            // C) Mostrar el nuevo formulario
             destino.Show();
 
-            // D) Menu nunca se cierra; los demás se liberan
             if (this is Menu)
-                this.Hide();     // se mantiene en memoria
+                this.Hide();
             else
-                this.Dispose();  // libera recursos
+                this.Dispose();
 
-            await Task.Delay(180); // Pausa opcional, transición suave
+            await Task.Delay(180);
         }
 
-        /* ── Aquí sigue tu lógica propia: botones, validaciones, etc. ── */
+        private void dgvPostulantes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Obtener la cédula del postulante seleccionado
+                cedulaPostulanteSeleccionado = dgvPostulantes.Rows[e.RowIndex].Cells["Cedula"].Value.ToString();
+
+            }
+        }
+
+        private void dgvEmpresas_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Obtener el correo de la empresa seleccionada
+                correoEmpresaSeleccionada = dgvEmpresas.Rows[e.RowIndex].Cells["Correo"].Value.ToString();
+
+            }
+        }
+
+        /* ── Aquí puedes seguir agregando más lógica propia ── */
     }
 }
