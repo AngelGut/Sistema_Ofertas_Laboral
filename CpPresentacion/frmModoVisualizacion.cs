6de03281b9d17f9875b77c5745c3695062eec2dc
Ángel David Gutierrez Contreras <angeldavid.guti.con@gmail.com>
2025-08-05 15:34:34 -0400

    miniformulario listo

---
+++ CpPresentacion/frmModoVisualizacion.cs
@@ -0,0 +1,46 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpPresentacion
+{
+    public partial class frmModoVisualizacion : MaterialForm
+    {
+        public enum ResultadoSeleccion { Ver, Editar, Cancelar }
+
+        public ResultadoSeleccion Resultado { get; private set; } = ResultadoSeleccion.Cancelar;
+
+        public frmModoVisualizacion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            Resultado = ResultadoSeleccion.Ver;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            Resultado = ResultadoSeleccion.Editar;
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Resultado = ResultadoSeleccion.Cancelar;
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
