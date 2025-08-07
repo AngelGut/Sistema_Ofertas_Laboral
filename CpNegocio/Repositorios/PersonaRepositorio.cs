c45710d483971deef0ca5d41d17989d28c3aa30e
David Paez <jeffersonpaez5@gmail.com>
2025-08-07 17:23:31 -0400

    Correccion en Repositorios como PersonaRepositorio

--- CpNegocio/Repositorios/PersonaRepositorio.cs
+++ CpNegocio/Repositorios/PersonaRepositorio.cs
@@ -11,36 +11,29 @@ using Microsoft.Data.SqlClient;
 
 namespace CpNegocio.Repositorios
 {
-    //TODO: Implementar la interfaz IPersonaRepositorio para la clase PersonaRepositorio
     public class PersonaRepositorio : IPersonaRepositorio
     {
-        //TODO: Implementar los métodos de la interfaz IPersonaRepositorio
         public Persona ObtenerPersonaPorCedula(string cedula)
         {
-            //TODO: Implementar la lógica para obtener una persona por su cédula
             using (SqlConnection connection = OfertaDatos.ObtenerConexion())
             {
-                //TODO: Abrir la conexión a la base de datos
                 connection.Open();
                 string query = "SELECT Id, Nombre, Dni, Correo, Telefono, Direccion FROM Persona WHERE Dni = @Dni";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Dni", cedula);
-
-                    //TODO: Ejecutar la consulta y leer los resultados
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         if (reader.Read())
                         {
-                            //TODO: Crear una instancia de Persona y asignar los valores leídos
                             return new Persona
                             {
-                                Id = reader.GetInt32(0),
-                                Nombre = reader.GetString(1),
-                                Dni = reader.GetString(2),
-                                Correo = reader.GetString(3),
-                                Telefono = reader.GetString(4),
-                                Direccion = reader.GetString(5)
+                                Id = (int)reader["Id"],
+                                Nombre = reader["Nombre"].ToString(),
+                                Dni = reader["Dni"].ToString(),
+                                Correo = reader["Correo"].ToString(),
+                                Telefono = reader["Telefono"].ToString(),
+                                Direccion = reader["Direccion"].ToString()
                             };
                         }
                     }
@@ -51,12 +44,10 @@ namespace CpNegocio.Repositorios
 
         public Persona ObtenerPersonaPorId(int id)
         {
-            // Ahora usa la clase OfertaDatos para obtener la conexión
             using (SqlConnection connection = OfertaDatos.ObtenerConexion())
             {
                 connection.Open();
-
-                string query = "SELECT Id, Dni, Nombre, Correo, Telefono FROM Persona WHERE Id = @Id";
+                string query = "SELECT Id, Dni, Nombre, Correo, Telefono, Direccion FROM Persona WHERE Id = @Id";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Id", id);
@@ -70,7 +61,8 @@ namespace CpNegocio.Repositorios
                                 Dni = reader["Dni"].ToString(),
                                 Nombre = reader["Nombre"].ToString(),
                                 Correo = reader["Correo"].ToString(),
-                                Telefono = reader["Telefono"].ToString()
+                                Telefono = reader["Telefono"].ToString(),
+                                Direccion = reader["Direccion"].ToString()
                             };
                         }
                     }
@@ -79,16 +71,13 @@ namespace CpNegocio.Repositorios
             return null;
         }
 
-
-        //TODO: Implementa la lógica SQL para obtener personas por área
         public List<Persona> ObtenerPersonasPorArea(string area)
         {
             List<Persona> personas = new List<Persona>();
             using (SqlConnection connection = OfertaDatos.ObtenerConexion())
             {
                 connection.Open();
-                //TODO: Esta consulta SQL obtiene una lista de personas que están asignadas a ofertas en un área específica
-                string query = @"SELECT DISTINCT p.Id, p.Nombre, p.Dni, p.Correo, p.Telefono FROM Persona p INNER JOIN Asignacion a ON p.Id = a.IdPersona INNER JOIN Oferta o ON a.IdOferta = o.Id WHERE o.Area = @Area";
+                string query = @"SELECT DISTINCT p.Id, p.Nombre, p.Dni, p.Correo, p.Telefono, p.Direccion FROM Persona p INNER JOIN Asignacion a ON p.Id = a.IdPersona INNER JOIN Oferta o ON a.IdOferta = o.Id WHERE o.Area = @Area";
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Area", area);
@@ -98,11 +87,12 @@ namespace CpNegocio.Repositorios
                         {
                             personas.Add(new Persona
                             {
-                                Id = reader.GetInt32(0),
-                                Nombre = reader.GetString(1),
-                                Dni = reader.GetString(2),
-                                Correo = reader.GetString(3),
-                                Telefono = reader.GetString(4),
+                                Id = (int)reader["Id"],
+                                Nombre = reader["Nombre"].ToString(),
+                                Dni = reader["Dni"].ToString(),
+                                Correo = reader["Correo"].ToString(),
+                                Telefono = reader["Telefono"].ToString(),
+                                Direccion = reader["Direccion"].ToString()
                             });
                         }
                     }
@@ -110,6 +100,5 @@ namespace CpNegocio.Repositorios
             }
             return personas;
         }
-
     }
 }
