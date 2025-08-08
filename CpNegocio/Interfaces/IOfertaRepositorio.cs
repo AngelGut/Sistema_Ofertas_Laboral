49c8fcc0379e210ba112812b52e5f0216e8ff714
David Paez <jeffersonpaez5@gmail.com>
2025-08-08 16:16:27 -0400

    Comentarios en las interfacez y en la clase PersonaRepositorio

--- CpNegocio/Interfaces/IOfertaRepositorio.cs
+++ CpNegocio/Interfaces/IOfertaRepositorio.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace CpNegocio.Oferta
 {
+    //TODO: Interfaz que define los métodos para interactuar con el repositorio de ofertas
     public interface IOfertaRepositorio
     {
+        //TODO: Este método devuelve una lista de ofertas disponibles
         List<CnOferta> ObtenerOfertasDisponibles();
+
+        //TODO: Un método encargado de obtener una oferta por su ID
         CnOferta ObtenerOfertaPorId(int idOferta);
     }
 }
