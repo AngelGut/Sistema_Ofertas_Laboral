e77461119a6707ffe2767a4768e160ccb5be7765
LUIS ALEJANDRO BRAVO BELLO <lb2024-1279@uce.edu.do>
2025-08-08 02:48:16 -0400

    Actualizacion filtros

--- CpPresentacion/cpRegistro.Designer.cs
+++ CpPresentacion/cpRegistro.Designer.cs
@@ -67,84 +67,84 @@
             materialTabControl1.Controls.Add(tabPage9);
             materialTabControl1.Depth = 0;
             materialTabControl1.Dock = DockStyle.Fill;
-            materialTabControl1.Location = new Point(2, 50);
+            materialTabControl1.Location = new Point(2, 38);
             materialTabControl1.Margin = new Padding(2, 2, 2, 2);
             materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
             materialTabControl1.Multiline = true;
             materialTabControl1.Name = "materialTabControl1";
             materialTabControl1.SelectedIndex = 0;
-            materialTabControl1.Size = new Size(895, 438);
+            materialTabControl1.Size = new Size(783, 327);
             materialTabControl1.TabIndex = 2;
             materialTabControl1.SelectedIndexChanged += materialTabControl1_SelectedIndexChanged;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 54);
+            tabPage1.Location = new Point(4, 24);
             tabPage1.Margin = new Padding(2, 2, 2, 2);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(2, 2, 2, 2);
-            tabPage1.Size = new Size(887, 380);
+            tabPage1.Size = new Size(775, 299);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Menu";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 29);
+            tabPage2.Location = new Point(4, 24);
             tabPage2.Margin = new Padding(2, 2, 2, 2);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(2, 2, 2, 2);
-            tabPage2.Size = new Size(886, 404);
+            tabPage2.Size = new Size(775, 300);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Ofertas Laborales";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 29);
+            tabPage3.Location = new Point(4, 24);
             tabPage3.Margin = new Padding(2, 2, 2, 2);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(886, 404);
+            tabPage3.Size = new Size(775, 300);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Empresas";
             tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            tabPage4.Location = new Point(4, 29);
+            tabPage4.Location = new Point(4, 24);
             tabPage4.Margin = new Padding(2, 2, 2, 2);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(886, 404);
+            tabPage4.Size = new Size(775, 300);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "Postulantes";
             tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            tabPage5.Location = new Point(4, 29);
+            tabPage5.Location = new Point(4, 24);
             tabPage5.Margin = new Padding(2, 2, 2, 2);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(886, 404);
+            tabPage5.Size = new Size(775, 300);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "Asignar Oferta";
             tabPage5.UseVisualStyleBackColor = true;
             // 
             // tabPage6
             // 
-            tabPage6.Location = new Point(4, 29);
+            tabPage6.Location = new Point(4, 24);
             tabPage6.Margin = new Padding(2, 2, 2, 2);
             tabPage6.Name = "tabPage6";
-            tabPage6.Size = new Size(886, 404);
+            tabPage6.Size = new Size(775, 300);
             tabPage6.TabIndex = 5;
             tabPage6.Text = "Historial Correos";
             tabPage6.UseVisualStyleBackColor = true;
             // 
             // tabPage7
             // 
-            tabPage7.Location = new Point(4, 29);
+            tabPage7.Location = new Point(4, 24);
             tabPage7.Margin = new Padding(2, 2, 2, 2);
             tabPage7.Name = "tabPage7";
-            tabPage7.Size = new Size(886, 404);
+            tabPage7.Size = new Size(775, 300);
             tabPage7.TabIndex = 6;
             tabPage7.Text = "Carnet";
             tabPage7.UseVisualStyleBackColor = true;
@@ -162,10 +162,10 @@
             tabPage8.Controls.Add(lblContraseña);
             tabPage8.Controls.Add(lblUsuario);
             tabPage8.Controls.Add(pictureBox1);
-            tabPage8.Location = new Point(4, 54);
+            tabPage8.Location = new Point(4, 24);
             tabPage8.Margin = new Padding(2, 2, 2, 2);
             tabPage8.Name = "tabPage8";
-            tabPage8.Size = new Size(887, 380);
+            tabPage8.Size = new Size(775, 299);
             tabPage8.TabIndex = 7;
             tabPage8.Text = "Registro Interno";
             tabPage8.UseVisualStyleBackColor = true;
@@ -173,10 +173,9 @@
             // btnCancelar
             // 
             btnCancelar.FlatStyle = FlatStyle.Flat;
-            btnCancelar.Location = new Point(529, 324);
-            btnCancelar.Margin = new Padding(3, 4, 3, 4);
+            btnCancelar.Location = new Point(463, 243);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(86, 30);
+            btnCancelar.Size = new Size(75, 23);
             btnCancelar.TabIndex = 10;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -185,10 +184,9 @@
             // btnRegistrar
             // 
             btnRegistrar.FlatStyle = FlatStyle.Flat;
-            btnRegistrar.Location = new Point(389, 324);
-            btnRegistrar.Margin = new Padding(3, 4, 3, 4);
+            btnRegistrar.Location = new Point(340, 243);
             btnRegistrar.Name = "btnRegistrar";
-            btnRegistrar.Size = new Size(86, 30);
+            btnRegistrar.Size = new Size(75, 23);
             btnRegistrar.TabIndex = 9;
             btnRegistrar.Text = "Guardar";
             btnRegistrar.UseVisualStyleBackColor = true;
@@ -198,71 +196,66 @@
             // 
             cmbRol.DropDownStyle = ComboBoxStyle.DropDownList;
             cmbRol.FormattingEnabled = true;
-            cmbRol.Location = new Point(477, 230);
-            cmbRol.Margin = new Padding(3, 4, 3, 4);
+            cmbRol.Location = new Point(417, 173);
             cmbRol.Name = "cmbRol";
-            cmbRol.Size = new Size(205, 28);
+            cmbRol.Size = new Size(180, 23);
             cmbRol.TabIndex = 8;
             // 
             // txtCorreo
             // 
-            txtCorreo.Location = new Point(477, 172);
-            txtCorreo.Margin = new Padding(3, 4, 3, 4);
+            txtCorreo.Location = new Point(417, 129);
             txtCorreo.Name = "txtCorreo";
-            txtCorreo.Size = new Size(205, 27);
+            txtCorreo.Size = new Size(180, 23);
             txtCorreo.TabIndex = 7;
             // 
             // txtContraseña
             // 
-            txtContraseña.Location = new Point(477, 126);
-            txtContraseña.Margin = new Padding(3, 4, 3, 4);
+            txtContraseña.Location = new Point(417, 95);
             txtContraseña.Name = "txtContraseña";
             txtContraseña.PasswordChar = '*';
-            txtContraseña.Size = new Size(205, 27);
+            txtContraseña.Size = new Size(180, 23);
             txtContraseña.TabIndex = 6;
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(477, 80);
-            txtUsuario.Margin = new Padding(3, 4, 3, 4);
+            txtUsuario.Location = new Point(417, 60);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(205, 27);
+            txtUsuario.Size = new Size(180, 23);
             txtUsuario.TabIndex = 5;
-            txtUsuario.KeyPress += txtUsuario_KeyPress;
             // 
             // blCorreo
             // 
             blCorreo.AutoSize = true;
-            blCorreo.Location = new Point(323, 176);
+            blCorreo.Location = new Point(283, 132);
             blCorreo.Name = "blCorreo";
-            blCorreo.Size = new Size(135, 20);
+            blCorreo.Size = new Size(108, 15);
             blCorreo.TabIndex = 4;
             blCorreo.Text = "Correo Electronico:";
             // 
             // lblRol
             // 
             lblRol.AutoSize = true;
-            lblRol.Location = new Point(323, 234);
+            lblRol.Location = new Point(283, 176);
             lblRol.Name = "lblRol";
-            lblRol.Size = new Size(34, 20);
+            lblRol.Size = new Size(27, 15);
             lblRol.TabIndex = 3;
             lblRol.Text = "Rol:";
             // 
             // lblContraseña
             // 
             lblContraseña.AutoSize = true;
-            lblContraseña.Location = new Point(323, 126);
+            lblContraseña.Location = new Point(283, 95);
             lblContraseña.Name = "lblContraseña";
-            lblContraseña.Size = new Size(86, 20);
+            lblContraseña.Size = new Size(70, 15);
             lblContraseña.TabIndex = 2;
             lblContraseña.Text = "Contraseña:";
             // 
             // lblUsuario
             // 
             lblUsuario.AutoSize = true;
-            lblUsuario.Location = new Point(323, 84);
+            lblUsuario.Location = new Point(283, 63);
             lblUsuario.Name = "lblUsuario";
-            lblUsuario.Size = new Size(62, 20);
+            lblUsuario.Size = new Size(50, 15);
             lblUsuario.TabIndex = 1;
             lblUsuario.Text = "Usuario:";
             // 
@@ -271,35 +264,34 @@
             pictureBox1.Dock = DockStyle.Left;
             pictureBox1.Image = Properties.Resources.Logo;
             pictureBox1.Location = new Point(0, 0);
-            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(245, 380);
+            pictureBox1.Size = new Size(214, 299);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // tabPage9
             // 
-            tabPage9.Location = new Point(4, 29);
+            tabPage9.Location = new Point(4, 24);
             tabPage9.Margin = new Padding(2, 2, 2, 2);
             tabPage9.Name = "tabPage9";
-            tabPage9.Size = new Size(886, 404);
+            tabPage9.Size = new Size(775, 300);
             tabPage9.TabIndex = 8;
             tabPage9.Text = "Historial Postulaciones";
             tabPage9.UseVisualStyleBackColor = true;
             // 
             // cpRegistro
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(899, 490);
+            ClientSize = new Size(787, 367);
             Controls.Add(materialTabControl1);
             DrawerTabControl = materialTabControl1;
             Margin = new Padding(2, 2, 2, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "cpRegistro";
-            Padding = new Padding(2, 50, 2, 2);
+            Padding = new Padding(2, 38, 2, 2);
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Registro";
             Load += cpRegistro_Load;
